c5e015f714edcc0be1397e297c0583b47afa4ab0
TitliChakraborty <93787434+TitliChakraborty@users.noreply.github.com>
2022-06-03 20:45:51 +0530

    Add files via upload

---
+++ Model Trainer.py
@@ -0,0 +1,58 @@
+import cv2
+import numpy as np
+from PIL import Image #pillow package
+import os
+
+# import pyttsx3
+
+# engine = pyttsx3.init('sapi5')
+
+# voices= engine.getProperty('voices') #getting details of current voice
+
+# engine.setProperty('voice', voices[0].id)
+
+# def speak(audio):
+#     engine.say(audio) 
+
+# engine.runAndWait() #Without this command, speech will not be audible to us.
+    #For now, we will write the conditions later.
+
+
+ 
+
+path = 'samples' # Path for samples already taken
+
+recognizer = cv2.face.LBPHFaceRecognizer_create() # Local Binary Patterns Histograms
+detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+#Haar Cascade classifier is an effective object detection approach
+
+
+def Images_And_Labels(path): # function to fetch the images and labels
+
+    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
+    faceSamples=[]
+    ids = []
+
+    for imagePath in imagePaths: # to iterate particular image path
+
+        gray_img = Image.open(imagePath).convert('L') # convert it to grayscale
+        img_arr = np.array(gray_img,'uint8') #creating an array
+
+        id = int(os.path.split(imagePath)[-1].split(".")[1])
+        faces = detector.detectMultiScale(img_arr)
+
+        for (x,y,w,h) in faces:
+            faceSamples.append(img_arr[y:y+h,x:x+w])
+            ids.append(id)
+
+    return faceSamples,ids
+
+print ("Training faces. It will take a few seconds. Wait ...")
+
+faces,ids = Images_And_Labels(path)
+recognizer.train(faces, np.array(ids))
+
+recognizer.write('trainer/trainer.yml')  # Save the trained model as trainer.yml
+
+print("Model trained, Now we can recognize your face.")
+#speak("model trained successfully")
